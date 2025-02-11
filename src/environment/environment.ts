14899849dafb1fb9c558d8840905857778e80ab6
sneejaOcp <ocphackathon2025@gmail.com>
2025-02-11 16:42:01 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://banking-portal-api-sneeja89-dev.apps.rm3.7wse.p1.openshiftapps.com/api',
+    apiUrl: process.env.API_URL,
     tokenName :  "authToken",
     origin:'http://localhost:4200' 
   };
